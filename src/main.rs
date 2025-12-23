954136ffb428a16329a06498809dd74965087301
BlacAmDK <BlacAmDK@gmail.com>
2025-12-23 14:32:42 +0800

    memory module update 1% instead of 1G, add core_affinity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::thread;
 use std::time::{Duration, Instant};
 use sysinfo::{MINIMUM_CPU_UPDATE_INTERVAL, MemoryRefreshKind, System};
@@ -5,8 +6,8 @@ use sysinfo::{MINIMUM_CPU_UPDATE_INTERVAL, MemoryRefreshKind, System};
 #[global_allocator]
 static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-const TARGET_CPU_USAGE: f32 = 70.0; // Target CPU usage percentage
-const TARGET_RAM_USAGE_RANGE: (u64, u64) = (55, 70); // Target RAM usage percentage range
+const TARGET_CPU_USAGE: f32 = 55.0; // Target CPU usage percentage
+const TARGET_RAM_USAGE_RANGE: (u64, u64) = (45, 55); // Target RAM usage percentage range
 const WORK_DURATION: Duration = MINIMUM_CPU_UPDATE_INTERVAL; // Duration of work
 const SLEEP_DURATION: Duration = MINIMUM_CPU_UPDATE_INTERVAL; // Duration of sleep
 
@@ -25,9 +26,10 @@ fn main() {
 
     // CPU Control
     for cpu_id in 0..System::new_all().cpus().len() {
-        if cpu_id <= 1 {
-            continue;
-        }
+        // if cpu_id <= 1 {
+        //     continue;
+        // }
+        core_affinity::set_for_current(core_affinity::CoreId { id: cpu_id });
         let handle = thread::spawn(move || {
             let mut system = System::new();
             system.refresh_cpu_usage();
@@ -67,6 +69,7 @@ struct Ram {
     target_usage_range: (u64, u64),
     target_usage_range_mid: u64,
     usage_percent: u64,
+    memory_one_percent: u64,
 }
 
 impl Ram {
@@ -80,6 +83,7 @@ impl Ram {
             target_usage_range: TARGET_RAM_USAGE_RANGE,
             target_usage_range_mid: (TARGET_RAM_USAGE_RANGE.0 + TARGET_RAM_USAGE_RANGE.1) / 2,
             usage_percent: system.used_memory() * 100 / system.total_memory(),
+            memory_one_percent: min(system.total_memory() / 100, 1024 * 1024 * 1024), // byte of 1% total_memory, 1G max
             system,
         }
     }
@@ -94,38 +98,38 @@ impl Ram {
             && self.usage_percent <= self.target_usage_range.1
         { // RAM usage in range, do nothing
         } else if self.usage_percent < self.target_usage_range.0 {
-            // allocate ram(add 1G per call)
+            // allocate ram(add 1% per call, up to 1G)
             let diff = self.system.total_memory() / 100
                 * (self.target_usage_range_mid - self.usage_percent)
-                / 1024
-                / 1024
-                / 1024;
+                / self.memory_one_percent;
             self.adjust_pool(diff.try_into().unwrap_or(0));
+            // println!("alloc--RAM:{}%, diff:{}", self.usage_percent, diff);
         } else {
             // free ram
             let diff = self.system.total_memory() / 100
                 * (self.usage_percent - self.target_usage_range_mid)
-                / 1024
-                / 1024
-                / 1024;
+                / self.memory_one_percent;
             self.adjust_pool(-(diff).try_into().unwrap_or(0));
+            // println!("free--RAM:{}%, diff:{}", self.usage_percent, diff);
         }
     }
     fn adjust_pool(&mut self, multiplier: i32) {
-        // if need allocate ram(multiplier>0), only allocate 1G
+        // if need allocate ram(multiplier>0), only allocate 1%
         // if need free ram(multiplier<0), free -multiplier times
         if multiplier > 0 {
             if let Some(block) = self.pool.first() {
                 self.pool.push(block.clone());
             } else {
-                let size: u32 = 1024 * 1024 * 1024 / 4; // size of 1GB u32
+                let size: u32 = (self.memory_one_percent / 4)
+                    .try_into()
+                    .unwrap_or(1024 * 1024 * 200 / 4); // size of 1% memory u32
                 let mut ram = Vec::with_capacity(size as usize);
                 for i in 0..size {
                     ram.push(i);
                 }
                 self.pool.push(ram);
             }
-        } else if multiplier < 0 {
+        } else if multiplier < 0 && !self.pool.is_empty() {
             for _ in 0..(-multiplier) {
                 self.pool.pop();
             }
